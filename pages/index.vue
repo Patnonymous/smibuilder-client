695beffc2cd44f38975f9ea031ff2f199cfcf59a
Patrick Williamson <patrickw9080@outlook.com>
2021-09-11 15:08:03 -0400

    init Added Nuxt boilerplate.

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Capstone Client Index</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data: function () {
+    return {
+      myNumber: 2,
+    };
+  },
+  mounted: function () {},
+  computed: {},
+  methods: {
+    goToOtherPageUsingButton: function () {
+      const TAG = "index - goToOtherPageUsingButton(), ";
+      console.log(TAG + "clicked and starting.");
+
+      this.$router.push({ path: "other-page" });
+    },
+  },
+};
+</script>
