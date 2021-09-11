51ab1f922054b5d69b34a6c84f50b1c7658f5a2d
Patrick Williamson <patrickw9080@outlook.com>
2021-09-11 15:13:51 -0400

    init Added dev-playground.

---
+++ pages/dev-playground.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Dev Playground Page</h1>
+        <input class="form-control" type="text" v-model="myInputMessage" />
+        <button class="btn btn-success" @click="printInputToConsole">
+          Print Message To Console
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data: function () {
+    return {
+      myInputMessage: "",
+    };
+  },
+  mounted: function () {},
+  computed: {},
+  methods: {
+    printInputToConsole: function () {
+      const TAG = "dev-playground - printInputToConsole(), ";
+      console.log(TAG + "The input message is: ", this.myInputMessage);
+    },
+  },
+};
+</script>
