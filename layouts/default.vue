06ca074b252cb1e05e0b56664c294c4f03191aee
Patrick Williamson <patrickw9080@outlook.com>
2021-09-15 11:48:01 -0400

    init Added default.vue with Nav Bar.

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NavBar />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import NavBar from "../components/NavigationHeader/NavBar.vue";
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
