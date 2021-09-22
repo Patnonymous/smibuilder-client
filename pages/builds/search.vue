f63502112b64d86c1ae617b4b4759b348576f8a7
Patrick Williamson <patrickw9080@outlook.com>
2021-09-22 12:29:30 -0400

    init Added boilerplate search page.

---
+++ pages/builds/search.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1>Builds - search</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "search",
+  components: {},
+  data: function () {
+    return {};
+  },
+  mounted: function () {},
+  computed: {},
+  methods: {},
+};
+</script>
