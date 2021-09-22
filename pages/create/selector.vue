0a14b5ae397096f4b4f99cc788686a25b3c76099
Patrick Williamson <patrickw9080@outlook.com>
2021-09-22 12:29:46 -0400

    init Added boilerplate create selector page.

---
+++ pages/create/selector.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1>Create - selector</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "selector",
+  components: {},
+  data: function () {
+    return {};
+  },
+  mounted: function () {},
+  computed: {},
+  methods: {},
+};
+</script>
