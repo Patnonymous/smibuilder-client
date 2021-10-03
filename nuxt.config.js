195d53a0cb6b8e4ff32ad98e6c0c999e0e2cdcba
Patrick Williamson <patrickw9080@outlook.com>
2021-10-03 18:06:22 -0400

    Init Added vue notifications.

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/notifications-ssr', ssr: true }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
