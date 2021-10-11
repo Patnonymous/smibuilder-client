b0786469068c2e68ef23d9077ba8d2a477bcc79a
Patrick Williamson <patrickw9080@outlook.com>
2021-10-11 10:33:25 -0400

    Update implemented basic login functions.

--- pages/login.vue
+++ pages/login.vue
@@ -27,10 +27,19 @@
               v-model="userInputPassword"
             />
           </div>
-          <NuxtLink to="/register">New user? Create Account</NuxtLink>
-          <button type="submit" class="btn btn-success m-2 float-right">
-            Log In
-          </button>
+
+          <p class="text-danger mt-1" v-if="errorMessage !== null">
+            {{ errorMessage }}
+          </p>
+
+          <div class="row">
+            <div class="col">
+              <NuxtLink to="/register">New user? Create Account</NuxtLink>
+              <button type="submit" class="btn btn-success m-2 float-right">
+                Log In
+              </button>
+            </div>
+          </div>
         </form>
       </div>
     </div>
@@ -40,11 +49,13 @@
 <script>
 export default {
   name: "login",
+  layout: "public",
   components: {},
   data: function () {
     return {
       userInputEmail: "",
       userInputPassword: "",
+      errorMessage: "",
     };
   },
   mounted: function () {},
@@ -52,15 +63,50 @@ export default {
   methods: {
     submitLogin: async function () {
       const TAG = "login - submitLogin(), ";
+      let userEmail = this.userInputEmail;
+      let userPassword = this.userInputPassword;
+      console.log(TAG + "Outputting inputs below: ");
+      console.log("userEmail: ", userEmail);
+      console.log("password: ", userPassword);
 
-      let loginResponse = await this.$axios.$post(
-        `${this.$config.serverUrl}/users`,
-        {
-          userName: this.userInputEmail,
-          password: this.userInputPassword,
+      this.errorMessage = "";
+
+      // Error checking.
+      if (userEmail.length === 0) {
+        this.errorMessage = "Email input was empty.";
+      } else if (userPassword.length === 0) {
+        this.errorMessage = "Password input was empty.";
+      } else {
+        console.log(TAG + "Passed basic error checks.");
+        let loginResponse = await this.$axios.$post(
+          `${this.$config.serverUrl}/users/login`,
+          {
+            email: userEmail,
+            password: userPassword,
+          }
+        );
+        console.log(TAG + "loginResponse: ", loginResponse);
+
+        if (loginResponse.status === "Failure") {
+          this.errorMessage = loginResponse.resData;
+          this.$notify({
+            title: "Login Error",
+            text: loginResponse.resData,
+            duration: 6000,
+            type: "error",
+          });
+        } else if (loginResponse.status === "Success") {
+          // Notify the user, commit their data to the store, then redirect.
+          this.$notify({
+            title: "Login",
+            text: "You are now logged in and authorized.",
+            duration: 6000,
+            type: "success",
+          });
+          this.$store.commit("user/setUser", loginResponse.resData);
+          this.$router.push({ path: "/builds/search" });
         }
-      );
-      console.log(TAG + "loginResponse: ", loginResponse);
+      }
     },
   },
 };
