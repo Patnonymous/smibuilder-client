a1313bd1fc70b40081c5d00b90d692a9f3b0ae35
Patrick Williamson <patrickw9080@outlook.com>
2021-10-11 10:34:10 -0400

    Init added public layout.

---
+++ layouts/public.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NavBar class="mb-4" />
+    <Nuxt />
+    <notifications position="bottom right" />
+  </div>
+</template>
+
+<script>
+import NavBar from "../components/NavigationHeader/NavBar.vue";
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
