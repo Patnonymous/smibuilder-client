e4919c982f5f6c48bebc17b1b8eab55318ec310c
Patrick Williamson <patrickw9080@outlook.com>
2021-10-13 16:03:56 -0400

    init WIP boilerplate admin pages.

---
+++ pages/admin/item-stats.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1>Admin - Item stats</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "item-stats",
+  layout: "default",
+  components: {},
+  data: function () {
+    return {};
+  },
+  mounted: function () {},
+  computed: {},
+  methods: {},
+};
+</script>
