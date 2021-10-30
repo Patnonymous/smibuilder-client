c6acec0325bbbdcdbdf750dd8e9e13702e34bb49
PatWStudentGit2000 <79163131+PatWStudentGit2000@users.noreply.github.com>
2021-10-30 16:58:03 -0400

    Added component that will display a Tag.

---
+++ components/Tags/GodTagPill.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="col p-1 bg-info rounded text-center">
+      <span>{{ tagLabel }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+/**
+ * @description Displays the Tag using a span inside a col.
+ */
+export default {
+  name: "GodTagPill",
+  components: {},
+  props: {
+    tagLabel: {
+      type: String,
+      required: true,
+    },
+  },
+  data: function () {
+    return {};
+  },
+  mounted: async function () {},
+  computed: {},
+  methods: {},
+};
+</script>
