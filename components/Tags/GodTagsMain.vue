3a476cbb16f1538f2c7ffd0a07bfed339a6cfc0a
PatWStudentGit2000 <79163131+PatWStudentGit2000@users.noreply.github.com>
2021-10-30 16:57:48 -0400

    Added component to display God Tag Pills.

---
+++ components/Tags/GodTagsMain.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="row justify-content-center">
+      <div class="col-auto">
+        <div class="row">
+          <GodTagPill
+            class="m-2"
+            v-for="item in godTags"
+            :tagLabel="item"
+            :key="item"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// Imports
+import GodTagPill from "./GodTagPill.vue";
+
+/**
+ * @description A row-col template that will display GodTagPill's according to the
+ * passed in godObject's info.
+ */
+export default {
+  name: "GodTagsMain",
+  components: {
+    GodTagPill,
+  },
+  props: {
+    godObject: {
+      type: Object,
+      required: true,
+    },
+    godDamageType: {
+      type: String,
+      required: true,
+    },
+    godBasicAttackType: {
+      type: String,
+      required: true,
+    },
+  },
+  data: function () {
+    return {
+      godTags: [],
+    };
+  },
+  mounted: async function () {
+    // First tag is pantheon
+    this.godTags.push(this.godObject.Pantheon);
+    // Second tag is role
+    this.godTags.push(this.godObject.Roles);
+    // Third is basic attack type
+    this.godTags.push(this.godBasicAttackType);
+    // Fourth is damage type
+    this.godTags.push(this.godDamageType);
+  },
+  computed: {},
+  methods: {},
+};
+</script>
