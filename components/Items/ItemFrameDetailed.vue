ea627018eb75c2ee3d957f596d1ae825111deb4c
PatWStudentGit2000 <79163131+PatWStudentGit2000@users.noreply.github.com>
2021-11-07 12:29:37 -0500

    init WIP a detailed Item Frame.

---
+++ components/Items/ItemFrameDetailed.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div v-if="itemData !== null" class="card" style="width: 18rem">
+      <img class="card-img-top" src="..." alt="Card image cap" />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+    <div v-else class="card" style="width: 18rem">
+      <h3>Current Selection: None.</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemFrameDetailed",
+  components: {},
+  props: {
+    /**
+     * @description Object containing item data. This is not required and defaults to null.
+     */
+    itemData: {
+      type: Object,
+      required: false,
+      default: null,
+    },
+  },
+  data: function () {
+    return {};
+  },
+  mounted: async function () {},
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style>
+</style>
