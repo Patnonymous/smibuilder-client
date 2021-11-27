d9c923c1a541dd35b393ccdbb26a8f50e4d39bfd
PatWStudentGit2000 <79163131+PatWStudentGit2000@users.noreply.github.com>
2021-11-27 15:30:03 -0500

    Update refactored the god panel into a reusable God Panel component.

--- pages/create/_godId.vue
+++ pages/create/_godId.vue
@@ -3,141 +3,10 @@
     <!-- Shows if validGod is true -->
     <div v-if="validGod !== false" class="container-fluid">
       <!-- Greater outer row will hold everything -->
-      <div class="row">
+      <div class="row ml-1 mr-1">
         <!-- Left side col will hold the god, their stats, level info etc. -->
         <div class="col-3 border border-primary">
-          <!-- First inner row will hold the card image and basic stats -->
-          <div class="row justify-content-center border border-danger">
-            <div class="col-auto border border-primary">
-              <GodCardLarge
-                :id="godObject.id"
-                :godName="godObject.Name"
-                :cardUrl="godObject.godCard_URL"
-              />
-            </div>
-
-            <div class="col">
-              <div class="row border">
-                <div class="col"><h3 class="align-middle">Level</h3></div>
-                <div class="col">
-                  <input
-                    class="form-control"
-                    type="number"
-                    min="1"
-                    max="20"
-                    v-model="currentLevel"
-                  />
-                </div>
-              </div>
-
-              <GodStatPill
-                statLabel="Health"
-                :statBase="godObject.Health"
-                :scalesWithLevel="true"
-                :scalingPerLevel="godObject.HealthPerLevel"
-                :currentLevel="currentLevelToInt"
-              />
-              <GodStatPill
-                statLabel="Mana"
-                :statBase="godObject.Mana"
-                :scalesWithLevel="true"
-                :scalingPerLevel="godObject.ManaPerLevel"
-                :currentLevel="currentLevelToInt"
-              />
-
-              <GodStatPill
-                v-if="godDamageType === 'Physical'"
-                statLabel="Physical Power"
-                :statBase="godObject.PhysicalPower"
-                :scalesWithLevel="true"
-                :scalingPerLevel="godObject.PhysicalPowerPerLevel"
-                :currentLevel="currentLevelToInt"
-              />
-              <GodStatPill
-                v-else
-                statLabel="Magical Power"
-                :statBase="godObject.MagicalPower"
-                :scalesWithLevel="true"
-                :scalingPerLevel="godObject.MagicalPowerPerLevel"
-                :currentLevel="currentLevelToInt"
-              />
-
-              <GodStatPill
-                statLabel="Physical Protection"
-                :statBase="godObject.PhysicalProtection"
-                :scalesWithLevel="true"
-                :scalingPerLevel="godObject.PhysicalProtectionPerLevel"
-                :currentLevel="currentLevelToInt"
-              />
-              <GodStatPill
-                statLabel="Magical Protection"
-                :statBase="godObject.MagicProtection"
-                :scalesWithLevel="true"
-                :scalingPerLevel="godObject.MagicProtectionPerLevel"
-                :currentLevel="currentLevelToInt"
-              />
-            </div>
-          </div>
-
-          <!-- Second inner row will hold the advanced stats -->
-          <div class="row justify-content-center border border-warning">
-            <div class="col">
-              <div class="row">
-                <div class="col border border-danger text-center">
-                  <h2>Advanced Stats</h2>
-                </div>
-              </div>
-
-              <GodStatPill
-                statLabel="Attack Speed"
-                :statBase="godObject.AttackSpeed"
-                :scalesWithLevel="true"
-                :scalingPerLevel="godObject.AttackSpeedPerLevel"
-                :currentLevel="currentLevelToInt"
-                :roundingDecimals="2"
-              />
-              <GodStatPill
-                statLabel="Attack Speed Per Level"
-                :statBase="godObject.AttackSpeedPerLevel"
-              />
-              <GodStatPill
-                statLabel="Health Per Level"
-                :statBase="godObject.HealthPerLevel"
-              />
-              <GodStatPill
-                statLabel="Mana Per Level"
-                :statBase="godObject.ManaPerLevel"
-              />
-              <GodStatPill
-                statLabel="HP5"
-                :statBase="godObject.HealthPerFive"
-                :scalesWithLevel="true"
-                :scalingPerLevel="godObject.HP5PerLevel"
-                :currentLevel="currentLevelToInt"
-                :roundingDecimals="2"
-              />
-              <GodStatPill
-                statLabel="HP5 Per Level"
-                :statBase="godObject.HP5PerLevel"
-              />
-              <GodStatPill
-                statLabel="MP5"
-                :statBase="godObject.ManaPerFive"
-                :scalesWithLevel="true"
-                :scalingPerLevel="godObject.MP5PerLevel"
-                :currentLevel="currentLevelToInt"
-                :roundingDecimals="2"
-              />
-              <GodStatPill
-                statLabel="MP5 Per Level"
-                :statBase="godObject.MP5PerLevel"
-              />
-              <GodStatPill
-                statLabel="Movement Speed"
-                :statBase="godObject.Speed"
-              />
-            </div>
-          </div>
+          <GodPanel :godObject="godObject" :godDamageType="godDamageType" />
         </div>
 
         <!-- Right side col will hold the tags, abilities, items, and publish buttons -->
@@ -256,8 +125,7 @@
 
 <script>
 // Imports
-import GodCardLarge from "../../components/Gods/GodCardLarge.vue";
-import GodStatPill from "../../components/Gods/GodStatPill.vue";
+import GodPanel from "../../components/Gods/GodPanel.vue";
 import GodTagsMain from "../../components/Tags/GodTagsMain.vue";
 import GodAbilitiesMain from "../../components/Abilities/GodAbilitiesMain.vue";
 import BuilderItemsMain from "../../components/Builder/BuilderItemsMain.vue";
@@ -273,8 +141,7 @@ export default {
   },
   layout: "public",
   components: {
-    GodCardLarge,
-    GodStatPill,
+    GodPanel,
     GodTagsMain,
     GodAbilitiesMain,
     BuilderItemsMain,
@@ -289,7 +156,6 @@ export default {
       /** True when the god data is loaded. */
       validGod: false,
       godObject: {},
-      currentLevel: 1,
       /** Magical or Physical */
       godDamageType: null,
       /** Melee or Ranged */
@@ -357,14 +223,7 @@ export default {
       loader.hide();
     }
   },
-  computed: {
-    /**
-     * @description Computed property to parse the level string to an Int.
-     */
-    currentLevelToInt: function () {
-      return parseInt(this.currentLevel);
-    },
-  },
+  computed: {},
   methods: {
     openPublishModal: function () {
       const TAG = "\n_godId - openPublishModal(), ";
