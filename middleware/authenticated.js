a7bfca8a16584daabafcca30499d5fd7d119fb4c
PatWStudentGit2000 <79163131+PatWStudentGit2000@users.noreply.github.com>
2021-11-28 13:14:30 -0500

    Update refactored to use async await.

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -4,30 +4,24 @@
  * If it verifies, authorized them.
  * @returns Redirection or nothing.
  */
-export default function ({ store, redirect, $axios, $config, $notify }) {
+export default async function ({ store, redirect, $axios, $config, $notify }) {
     const TAG = "\nmiddleware - authenticated(), ";
     console.log(TAG + "Outputting user state: ");
     console.log(store.state.user);
-    console.log("Outputting localstorage auth: ");
-    console.log(localStorage.getItem("auth"));
-
-    // DEBUG
-    //localStorage.removeItem("auth")
-    //console.log(localStorage.getItem("auth"));
-
 
     if (localStorage.getItem("auth") === null) { // no token
-        console.log(TAG + "User has no token.");
+        console.log("User has no token.");
         return redirect("/login");
     } else {
-        console.log(TAG + "User has token.")
+        console.log("User has token.")
         const token = localStorage.getItem("auth");
-        $axios.post(`${$config.serverUrl}/users/verify`, { token: token }).then(res => {
-            console.log(TAG + "res data: ");
-            console.dir(res.data);
 
-            if (res.data.status === "Failure") { // Failure to verify the token, redirect to login.
-                console.log(TAG + "Verify status == Failure");
+        try {
+            let verifyResponse = await $axios.post(`${$config.serverUrl}/users/verify`, { token: token });
+            let data = verifyResponse.data;
+
+            // Failure to verify the token, redirect to login.
+            if (data.status === "Failure") {
                 $notify({
                     title: "Authentication Error",
                     text: `An error has occurred: '${res.data.resData}'`,
@@ -37,17 +31,13 @@ export default function ({ store, redirect, $axios, $config, $notify }) {
                 localStorage.removeItem("auth");
                 this.$store.commit("user/logOut");
                 return redirect("/login");
-            } else if (res.data.status === "Success") { // Successful verification, commit authorization.
-                console.log(TAG + "Verify status == Success");
-                store.commit("user/setUser", res.data.resData);
-            };
-
-
-        }).catch(error => {
-            console.log(TAG + "Error catched on verify: ");
+            } else if (data.status === "Success") {
+                store.commit("user/setUser", data.resData);
+            }
+        } catch (error) {
+            console.log("ERROR: ");
             console.log(error.message);
-            console.log(error);
             return redirect("/login");
-        });
+        }
     };
 };
