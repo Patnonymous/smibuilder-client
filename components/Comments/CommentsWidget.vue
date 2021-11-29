c9ed90c74013c39e0ff3905619b778d231c74a5e
PatWStudentGit2000 <79163131+PatWStudentGit2000@users.noreply.github.com>
2021-11-29 17:40:14 -0500

    Added sorting functionality to comments.

--- components/Comments/CommentsWidget.vue
+++ components/Comments/CommentsWidget.vue
@@ -30,6 +30,67 @@
       </div>
     </div>
 
+    <div class="row p-1 m-2 mb-1 mt-4 bg-light rounded">
+      <div class="col-3">
+        <span class="text-dark">Sort: </span>
+        <b-dropdown
+          id="dropdown-1"
+          :text="currentSortType"
+          variant="primary"
+          class="ml-4"
+        >
+          <b-dropdown-item @click="changeSort('Date Added')"
+            >Date Added</b-dropdown-item
+          >
+          <b-dropdown-item @click="changeSort('Rating')"
+            >Rating</b-dropdown-item
+          >
+        </b-dropdown>
+
+        <!-- These two buttons toggle Ascending or Descending sort. -->
+        <button
+          v-if="sortAscending"
+          @click="sortAscending = false"
+          title="Ascending"
+          type="button"
+          class="btn btn-primary"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-sort-up"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M3.5 12.5a.5.5 0 0 1-1 0V3.707L1.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.498.498 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 3.707V12.5zm3.5-9a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z"
+            />
+          </svg>
+        </button>
+        <button
+          v-else
+          @click="sortAscending = true"
+          title="Descending"
+          type="button"
+          class="btn btn-primary"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-sort-down"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M3.5 2.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 11.293V2.5zm3.5 1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+
     <!-- Row for comments display section. -->
     <div
       v-if="commentsReady"
@@ -39,7 +100,7 @@
         <Comment
           @commentDeleted="removeCommentFromArray"
           class="bg-light rounded text-dark"
-          v-for="(comment, index) in arrayComments"
+          v-for="(comment, index) in filteredComments"
           :key="comment.id"
           :commentIndex="index"
           :commentData="comment"
@@ -69,12 +130,45 @@ export default {
       commentsReady: false,
       userInputComment: "",
       errorMessage: null,
+      // Sorting vars.
+      currentSortType: "Date Added",
+      sortAscending: false,
     };
   },
   mounted: async function () {
     this.getAllComments();
   },
-  computed: {},
+  computed: {
+    filteredComments: function () {
+      let filteredComments = [];
+      if (this.currentSortType === "Date Added") {
+        filteredComments = this.arrayComments.sort((a, b) => {
+          if (a.createdOn < b.createdOn) {
+            return 1;
+          } else if (a.createdOn > b.createdOn) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
+      } else if (this.currentSortType === "Rating") {
+        filteredComments = this.arrayComments.sort((a, b) => {
+          if (a.rating < b.rating) {
+            return 1;
+          } else if (a.rating > b.rating) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
+      }
+
+      if (this.sortAscending) {
+        filteredComments.reverse();
+      }
+      return filteredComments;
+    },
+  },
   methods: {
     getAllComments: async function () {
       this.arrayComments.length = 0;
@@ -140,6 +234,9 @@ export default {
     },
     removeCommentFromArray: function (index) {
       this.arrayComments.splice(index, 1);
+    },
+    changeSort: function (sortType) {
+      this.currentSortType = sortType;
     },
   },
 };
